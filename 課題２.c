317f02017a299e6feef52c27c91a6bf125342e2a
Toi <t246552@st.yamagata-u.ac.jp>
2025-10-17 19:29:43 +0900

    Add files via upload

--- 課題２.c
+++ 課題２.c
@@ -2,8 +2,11 @@
 
 int main()
 {
-    printf("How many pens");
-    scanf("%d",&k)
-    printf("The number of pens is %d,k");
+   int k;
+    printf("How many pens?");
+    scanf("%d", &k);
+    
+    printf("The number of pens is %d",k);
+    
     return 0;
 }
