4be8469840554f357ddf19d18953846d52c1ed2c
Toi <t246552@st.yamagata-u.ac.jp>
2025-10-24 19:20:09 +0900

    Add files via upload

---
+++ 課題３.c
@@ -0,0 +1,12 @@
+#include<float.h>
+#include<limits.h>
+#include <stdio.h>
+
+int main(void)
+{
+    printf("double型の最大値:%u\n",DBL_MAX);
+    printf("double型の最小値:%e\n",DBL_MIN);
+    printf("double precision:%lli\n",DBL_DIG);
+    
+    return 0;
+}
